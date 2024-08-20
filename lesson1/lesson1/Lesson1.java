6fb6f720869a465d00edd11d6432d2e5a96a9c62
mksung89 <mksung89@gmail.com>
2024-08-20 18:24:13 +0900

    -source codes

---
+++ lesson1/lesson1/Lesson1.java
@@ -0,0 +1,18 @@
+
+/**
+ * Lesson1 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class Lesson1
+{
+    
+    public Lesson1()
+    {
+        // 인스턴스 변수 초기화
+        System.out.println("hello world");
+    }
+
+    
+}
